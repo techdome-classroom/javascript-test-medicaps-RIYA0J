dce8c60cfc21b8db3a18aef42e3f6023ca28246e
RIYA0J <riyajaisinghani5@gmail.com>
2024-04-25 13:49:30 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,26 @@
 function smallestMissingPositiveInteger(nums) {
+  const n = nums.length;
 
-  // Implement the function smallest_missing_positive_integer
+  for (let i = 0; i < n; i++) {
+    if (nums[i] <= 0 || nums[i] > n) {
+      nums[i] = 0;
+    }
+  }
 
+  for (let i = 0; i < n; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    if (index < n && nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] >= 0) {
+      return i + 1;
+    }
+  }
+
+  return n + 1; // If all positive integers up to n are present
 }
 
 module.exports = smallestMissingPositiveInteger;
