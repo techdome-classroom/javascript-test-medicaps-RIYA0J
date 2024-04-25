5ce29e2fc788b1d6142df8399da8e23f370d9ebd
RIYA0J <riyajaisinghani5@gmail.com>
2024-04-25 13:49:11 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,17 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
-}
-
-module.exports = { longestSubstring };
-
-
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
+  
+    for (let i = 0; i < s.length; i++) {
+      if (charIndexMap.hasOwnProperty(s[i]) && charIndexMap[s[i]] >= start) {
+        start = charIndexMap[s[i]] + 1;
+      }
+      charIndexMap[s[i]] = i;
+      maxLength = Math.max(maxLength, i - start + 1);
+    }
+  
+    return maxLength;
+  }
+  
+  module.exports = { longestSubstring };
